12e0ec5250f0059f7c4ed91e033e4e1d7fac9bb2
Dekadenca <emil.avdic@viberate.com>
2024-03-10 19:03:24 +0100

    initial commit

---
+++ CircularSlider.js
@@ -0,0 +1,10 @@
+function circularSlider(options) {
+  var container = options.container;
+  var color = options.color;
+  var max = options.max;
+  var min = options.min;
+  var step = options.step;
+  var radius = options.radius;
+
+  return;
+}
