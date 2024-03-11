01e439dd382678da60898a09254d82b1c35c400e
Dekadenca <emil.avdic@viberate.com>
2024-03-11 19:00:02 +0100

    add angle calculation

--- CircularSlider.js
+++ CircularSlider.js
@@ -31,17 +31,50 @@ function circularSlider({
 function onKnobDrag(event) {
   event.preventDefault();
 
+  let dragFunction = knobDragging.bind(this);
+
   this.classList.add("dragging");
-  addEventListener("mousemove", knobDragging);
+  addEventListener("mousemove", dragFunction);
   document.body.classList.add("dragging");
 
   addEventListener("mouseup", () => {
-    removeEventListener("mousemove", knobDragging);
+    removeEventListener("mousemove", dragFunction);
     this.classList.remove("dragging");
     document.body.classList.remove("dragging");
   }, {once: true});
 }
 
 function knobDragging(event) {
-  console.log(event.pageX, event.pageY);
+  //Mouse coordinates
+  let mouseX = event.pageX;
+  let mouseY = event.pageY;
+
+  //Container center coordinates
+  let containerBoundingRect = this.parentNode.getBoundingClientRect();
+  let containerX = (containerBoundingRect.width / 2) + containerBoundingRect.x;
+  let containerY = (containerBoundingRect.height / 2) + containerBoundingRect.y;
+
+  //Calculation to get angle of the mouse position relative to the containers center.
+  let relativeX = mouseX - containerX;
+  let relativeY = mouseY - containerY;
+  let theta = Math.atan2(relativeY, relativeX);
+
+
+  //First quadrant
+  if (relativeX >= 0 && relativeY <= 0) {
+    theta += Math.PI / 2;
+    //Second quadrant
+  } else if (relativeX >= 0 && relativeY >= 0) {
+    theta += Math.PI / 2;
+    //Third quadrant
+  } else if (relativeX <= 0 && relativeY >= 0) {
+    theta += Math.PI / 2;
+    //Fourth quadrant
+  } else if (relativeX <= 0 && relativeY <= 0) {
+    theta += 2 * Math.PI + (Math.PI / 2);
+  }
+
+  theta = (theta * 180) / Math.PI;
+
+  console.log(theta);
 }
