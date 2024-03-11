e2de0bdc382faa2ab4cc18324a41e261d9b28b67
Dekadenca <emil.avdic@viberate.com>
2024-03-11 16:17:11 +0100

    Set basic elements and basic events:
    * add container with border and circular look
    * add knob with drag listeners

--- CircularSlider.js
+++ CircularSlider.js
@@ -1,10 +1,47 @@
-function circularSlider(options) {
-  var container = options.container;
-  var color = options.color;
-  var max = options.max;
-  var min = options.min;
-  var step = options.step;
-  var radius = options.radius;
+function circularSlider({
+  container,
+  color = "red",
+  max = 1000,
+  min = 0,
+  step = 50,
+  radius = 250,
+}) {
+
+  let mainContainer = document.createElement("div");
+  mainContainer.style.position = "absolute";
+
+  let slider = document.createElement("div");
+  slider.classList.add("circularSlider");
+  slider.style.width = radius;
+  slider.style.height = radius;
+  mainContainer.appendChild(slider);
+
+  let knob = document.createElement("div");
+  knob.classList.add("knob");
+  knob.onmousedown = onKnobDrag;
+  mainContainer.appendChild(knob);
+
+
+
+  document.querySelector(container).appendChild(mainContainer);
 
   return;
+}
+
+function onKnobDrag(event) {
+  event.preventDefault();
+
+  this.classList.add("dragging");
+  addEventListener("mousemove", knobDragging);
+  document.body.classList.add("dragging");
+
+  addEventListener("mouseup", () => {
+    removeEventListener("mousemove", knobDragging);
+    this.classList.remove("dragging");
+    document.body.classList.remove("dragging");
+  }, {once: true});
+}
+
+function knobDragging(event) {
+  console.log(event.pageX, event.pageY);
 }
